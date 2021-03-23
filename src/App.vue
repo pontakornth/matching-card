8216a6af4ee4ef7d8a438379dab5eebc8d467dde
pontakornth <pontakorn61@gmail.com>
2021-03-23 16:30:52 +0700

    feat: Make it clickable

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
 <template>
-<h1 class="text-5xl text-center font-bold">Hello World</h1>
-<div class="grid grid-cols-5 gap-4 p-8 container mx-auto">
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
-<card text="Hihi"></card>
+<h1 class="text-5xl text-center font-bold">Memory Game</h1>
+<div class="grid grid-cols-4 gap-4 p-8 container mx-auto">
+<card v-for="name in names" :key="name" :text="name" ></card>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import Card from './components/Card.vue'
+// Credit: https://stackoverflow.com/a/2450976/10978978
+function shuffle<T>(array: T[]) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 export default defineComponent({
   name: 'App',
   components: {
     Card
+  },
+  setup() {
+    const nameArray = ["Asta", "Yuno", "Noelle", "Most", "Yami", "Cream", "Shadow", "Licht"]
+    const firstArray: string[] = shuffle(nameArray)
+    const secondArray: string[] = shuffle(nameArray)
+    const names = reactive([...firstArray, ...secondArray])
+    return {
+      names
+    }
   }
 })
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="text-center shadow-md border bg-red-500 text-white border rounded p-4">
-        {{text}}
-    </div>
+    <button @click="handleClick" class="text-center shadow-md border bg-red-500 text-white border rounded p-4">
+        <span v-if="isOpen">{{text}}</span>
+        <span v-else>XXX</span>
+    </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     name: 'Card',
@@ -13,6 +14,14 @@ export default defineComponent({
         text: {
             type: String,
             required: true
+        }
+    },
+    setup() {
+        const isOpen = ref(false)
+        const handleClick = () => { isOpen.value = !isOpen.value }
+        return {
+            isOpen,
+            handleClick
         }
     }
 })
