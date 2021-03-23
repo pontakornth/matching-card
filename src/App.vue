1303aaf1b6bf781b1eef268cad524aa47d732e7b
pontakornth <pontakorn61@gmail.com>
2021-03-23 15:59:05 +0700

    feat: Complete initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
-<h1 class="text-5xl font-bold">Hello World</h1>
+<h1 class="text-5xl text-center font-bold">Hello World</h1>
+<div class="grid grid-cols-5 gap-4 p-8 container mx-auto">
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+<card text="Hihi"></card>
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Card from './components/Card.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Card
+  }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  @apply bg-gray-100 p-8;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center shadow-md border bg-red-500 text-white border rounded p-4">
+        {{text}}
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Card',
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
